f4b30f321a0cfa8b7b175fa9c78c4a928c0c7373
Alfonso <alfonsomanuel2000@gmail.com>
2024-06-04 11:23:36 +0200

    deploy

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
+
+function Home(){
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <div>
+
+                <button>
+
+                </button>
+            
+            </div>
+        </div>
+    )
+}
+
+
+export default Home
